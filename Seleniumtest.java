3a5ad39565ed2bcfb4f2ee0d8de4c984452f22e2
shaamilsiraj <post2shamil@gmail.com>
2015-02-02 16:27:31 +0530

    Update Seleniumtest.java

--- Seleniumtest.java
+++ Seleniumtest.java
@@ -3,9 +3,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import java.util.concurrent.TimeUnit;
+
 
 public class Seleniumtest {
 	WebDriver driver;
@@ -22,46 +22,60 @@ public class Seleniumtest {
 	public void testWindowAlert() throws Exception{
 
 		// enter a valid email address
-		driver.findElement(By.id("Email")).sendKeys("seleniumtest567@gmail.com");
+		WebElement Email = driver.findElement(By.id("Email"));
+		Email.sendKeys("seleniumtest567@gmail.com");
 
 		// enter a valid password
-		driver.findElement(By.id("Passwd")).sendKeys("automation");
+		WebElement Password = driver.findElement(By.id("Passwd"));
+		Password.sendKeys("automation");
 
 		// click on sign in button
-		driver.findElement(By.id("signIn")).click();
+		WebElement SignIn = driver.findElement(By.id("signIn"));
+		SignIn.click();
 		Thread.sleep(15000);
 
 		// click on compose button
-		driver.findElement(By.xpath("//div[@class='z0']//div[contains(text(),'COMPOSE')]")).click();
+		WebElement Compose = driver.findElement(By.xpath("//div[@class='z0']//div[contains(text(),'COMPOSE')]"));
+		Compose.click();
+		
+		// enter another email id
+		WebElement ToAdd = driver.findElement(By.xpath("//*[@id=':h3']"));
+		ToAdd.sendKeys("hr@cinglevue.com");
+		
+		// enter a subject for the mail
+		WebElement Typesub = driver.findElement(By.name("subjectbox"));
+		Typesub.sendKeys("CingleVue_QA_Challenge");
 
-		// click on attach files icon
-		//driver.findElement(By.xpath("//div[contains(@command,'Files')]//div[contains(@class,'aaA')]")).click();
-
-
-		driver.findElement(By.name("subjectbox")).sendKeys("cinglevue_QA_Challenge");
-
-		//driver.findElement(By.cssSelector("textarea[class='wO nr l1']")).sendKeys("another.email@somedomain.com");
-
-		driver.findElement(By.cssSelector("div[class='Am Al editable LW-avf']")).sendKeys("Testing123"); 
-
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-		driver.findElement(By.cssSelector("img[class='Ha']")).click();
-
-
-		driver.findElement(By.xpath("//*[@id=':55']/div/div[1]/span/a")).click();
+		// enter some text in message area
+		WebElement Typemsg = driver.findElement(By.cssSelector("div[class='Am Al editable LW-avf']"));
+		Typemsg.sendKeys("CingleVue Technical QA Challenge"); 
+		Thread.sleep(1000);
+		
+		// click on close icon
+		WebElement Composeclose = driver.findElement(By.cssSelector("img[class='Ha']"));
+		Composeclose.click();
+		Thread.sleep(1000);
+		
+		// click on the draft link
+		WebElement Draft = driver.findElement(By.xpath("//*[@id=':55']/div/div[1]/span/a"));
+		Draft.click();
 		Thread.sleep(2000);
 
-
-		driver.findElement(By.xpath("//*[@id='gb']/div[1]/div[1]/div/div[3]/div[1]/a")).click();
+		// click on the user drop down
+		WebElement user = driver.findElement(By.xpath("//*[@id='gb']/div[1]/div[1]/div/div[3]/div[1]/a"));
+		user.click();
 		Thread.sleep(500);
-		driver.findElement(By.id("gb_71")).click();
+		
+		// click on sign out 
+		WebElement Signout = driver.findElement(By.id("gb_71"));
+		Signout.click();
 
 	}
 
 	@After
 	public void tearDown()
 	{
+		// browser will close by exiting the driver
 		driver.close();
 	}
 }
